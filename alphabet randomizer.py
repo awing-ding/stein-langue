fce151783eebae6f3127a8f8f3b45884c2419fb4
awing-ding <66324442+awing-ding@users.noreply.github.com>
2023-03-25 16:35:12 +0100

    init

---
+++ alphabet randomizer.py
@@ -0,0 +1,14 @@
+import random
+
+PATH = "alphabet.txt"
+
+def randomizer(path: str)-> None:
+    with open(PATH, "r") as file:
+        text = file.read()
+        text = text.split("\n")
+        random.shuffle(text)
+        text = "\n".join(text)
+    with open(PATH, "w") as file:
+        file.write(text)
+
+randomizer(PATH)
